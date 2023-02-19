2d220ba3eade8ac0486b96cd3d300f16b421a5a6
mehelborn <mr.4uldum@gmail.com>
2023-02-19 22:53:54 +0700

    -started syling the thank-you-state
    -added js code, event listener on click on submit button

---
+++ index.js
@@ -0,0 +1,4 @@
+document.querySelector(".submit").addEventListener("click", function() {
+    document.querySelector(".rate-state").classList.add("hide");
+    document.querySelector(".ty-state").classList.remove("hide");
+});
