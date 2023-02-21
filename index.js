68ec937e1c10639595b7dd7b386b7aa29cda0961
mehelborn <mr.4uldum@gmail.com>
2023-02-21 20:52:46 +0700

    finished the project
    -replaced the rating buttons with input/label radio buttons
    -added event listeners to the rating buttons and for submit button in js file
    -added more css code for other screen resolutions

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-document.querySelector(".submit").addEventListener("click", function() {
-    document.querySelector(".rate-state").classList.add("hide");
-    document.querySelector(".ty-state").classList.remove("hide");
+var chosenRate = 0;
+
+document.querySelectorAll(".grades").forEach((element) => {
+  element.addEventListener("click", function () {
+    chosenRate = element.value;
+  });
+});
+
+document.querySelector(".submit").addEventListener("click", function () {
+  document.querySelector(".rate-state").classList.add("hide");
+  document.querySelector(".ty-state").classList.remove("hide");
+  document.getElementById("rate").textContent = chosenRate;
+  document.getElementById("rate").classList.remove("hide");
 });
